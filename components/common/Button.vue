bb7c4a86beb70232bea2991eec25c9570bd7843f
glaucobluedev <glauco@sounuv.com>
2025-06-06 07:04:40 -0300

    feat: Button component

---
+++ components/common/Button.vue
@@ -0,0 +1,15 @@
+<!-- components/common/BaseButton.vue -->
+<template>
+  <button
+    :type="type"
+    class="w-full bg-indigo-500 text-white py-2 rounded-md hover:bg-indigo-600 font-semibold"
+  >
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  type?: 'submit' | 'button'
+}>()
+</script>
