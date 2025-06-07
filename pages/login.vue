d06676cdd45c899c8eee5dcca2f7d943d7f06c12
glaucobluedev <glauco@sounuv.com>
2025-06-06 21:23:33 -0300

    feat: implementing toast on login page

--- pages/login.vue
+++ pages/login.vue
@@ -11,17 +11,23 @@ import { login } from '~/clients/axios'
 import { useRouter } from 'vue-router'
 import FormLogin from '~/components/FormLogin.vue'
 import { useAuthStore } from '~/stores/auth'
+import { useToast } from 'vue-toastification'
 
 const auth = useAuthStore()
 const router = useRouter()
 const email = ref('')
 const password = ref('')
+const toast = useToast()
 
 const handleLogin = async () => {
-  const response = await login(email.value, password.value) as { accessToken: string }
+  try {
+    const response = await login(email.value, password.value) as { accessToken: string }
+    auth.setAccessToken(response.accessToken)
+    toast.success(`Seja bem-vindo, ${auth.user.name}`)
+    router.push('/dashboard')
+  } catch (err: any) {
+    toast.error(err.message || 'Erro ao fazer login')
+  }
 
-  auth.setAccessToken(response.accessToken)
-
-  router.push('/dashboard')
 }
 </script>
